ee1222f270b670a0e90651c68aee43beb88f60a4
Reaper7861 <immortalsoul7861@gmail.com>
2025-06-10 14:24:43 -0500

    created and included empty pages

---
+++ frontend/src/pages/EventManagementPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EventManagementPage() {
+    return <h2>Event Management Page</h2>;
+}
+
+export default EventManagementPage;
---
+++ frontend/src/pages/MatchPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MatchPage() {
+    return <h2>Match Page</h2>;
+}
+
+export default MatchPage;
