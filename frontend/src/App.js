c6314c1fcf0ca4d2a15d493da4f0481aba439bf7
Reaper7861 <immortalsoul7861@gmail.com>
2025-06-10 14:24:43 -0500

    created and included empty pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import ProfilePage from './pages/ProfilePage'
+import EventManagementPage from './pages/EventManagementPage'
+import MatchPage from './pages/MatchPage'
+import NotificationsPage from './pages/NotificationsPage'
+import HistoryPage from './pages/HistoryPage'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Volunteer App</h1>
+    <div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/events" element={<EventManagementPage />} />
+        <Route path="/matching" element={<MatchPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/NotificationsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotificationsPage() {
+    return <h2>Notifications Page</h2>;
+}
+
+export default NotificationsPage;
