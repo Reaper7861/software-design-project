2050f235997f45a1478191fc659773838eeaabb4
Reaper7861 <immortalsoul7861@gmail.com>
2025-06-10 14:43:48 -0500

    included empty navbar and auth

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav style={{ backgroundColor: '#2c3e50', color: 'white', padding: '1rem'}}>
+            <h2>Volunteer Hub</h2>
+        </nav>
+    );
+};
+
+export default Navbar;
