f68380926caf160b3080e765e1cf0e611854bafe
Reaper7861 <immortalsoul7861@gmail.com>
2025-06-13 17:42:31 -0500

    added buttons for login and register pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,8 +3,10 @@ import { Button } from '@mui/material'
 
 const Navbar = () => {
     return (
-        <nav style={{ backgroundColor: '#2c3e50', color: 'white', padding: '1rem'}}>
+        <nav style={{ backgroundColor: '#502c2c', color: 'white', padding: '1rem'}}>
             <h2>Volunteer Homepage</h2>
+            <Button href="/login">Login</Button>
+            <Button href="/register">Register</Button>
             <Button href="/notifications">Notifications</Button>
             <Button href="/history">Volunteer History</Button>
         </nav>
