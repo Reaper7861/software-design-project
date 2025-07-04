63d821d8b4fdcd6fdda3683ce8fee9c53250a72d
Reaper7861 <immortalsoul7861@gmail.com>
2025-07-04 01:34:36 -0500

    added comments

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -2,17 +2,21 @@ import {useAuth} from "../contexts/AuthContext";
 import {Navigate} from "react-router-dom";
 
 
+// Allow adminstrators access 
 export const AdminRoute = ({children}) => {
+    // Get logged-in user
     const {user} = useAuth();
 
+    // If not logged, redirect to login page
     if(!user){
         return <Navigate to = "/login" replace/>;
     }
 
+    // If logged in, but not admin, redirect to profile page (volunteers)
     if(user.role !== "administrator"){
         return <Navigate to = "/profile" replace />;
     }
 
-
+    // If admin, render child components
     return children;
 };
