753cade8b7f83652e73b158e638abf8cb35bab4d
Reaper7861 <immortalsoul7861@gmail.com>
2025-07-04 12:59:45 -0500

    updated regex

--- backend/src/utils/validators.js
+++ backend/src/utils/validators.js
@@ -10,7 +10,7 @@ const isValidPassword = (password) => {
         return false; 
     }
 
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
     return passwordRegex.test(password);
 };
 
